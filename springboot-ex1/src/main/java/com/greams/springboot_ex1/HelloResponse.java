1a075b199fcc57b7a2aa54dcd6fc76c9b596e1cf
Gamika Jayawardana <gamikakj@gmail.com>
2026-03-08 14:50:32 +0530

    Add HelloResponse DTO and name endpoint
    Introduce a HelloResponse DTO and update HelloController to return it instead of plain strings. Adds a new GET /hello/{name} endpoint that returns a personalized HelloResponse and changes GET /hello to return HelloResponse as well. Imports consolidated to use a wildcard for Spring Web annotations.

---
+++ springboot-ex1/src/main/java/com/greams/springboot_ex1/HelloResponse.java
@@ -0,0 +1,17 @@
+package com.greams.springboot_ex1;
+
+public class HelloResponse {
+    private String message;
+
+    public HelloResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
